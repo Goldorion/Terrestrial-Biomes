b4d4fbb94aa2e9c72771e434b6800c42f951b10d
Max094Reikeb <maxencebekier@gmail.com>
2022-04-14 17:45:30 +0200

    Sorted registries

--- src/main/java/net/golkeb/terrestrial_biomes/setup/RegistryHandler.java
+++ src/main/java/net/golkeb/terrestrial_biomes/setup/RegistryHandler.java
@@ -1,16 +1,13 @@
 package net.golkeb.terrestrial_biomes.setup;
 
-import net.golkeb.terrestrial_biomes.init.BlockInit;
-import net.golkeb.terrestrial_biomes.init.ContainerInit;
-import net.golkeb.terrestrial_biomes.init.ItemInit;
-import net.golkeb.terrestrial_biomes.init.SoundsInit;
+import net.golkeb.terrestrial_biomes.init.*;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 public class RegistryHandler {
 
     public static void init() {
-        ContainerInit.CONTAINERS.register(FMLJavaModLoadingContext.get().getModEventBus());
         BlockInit.BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        ContainerInit.CONTAINERS.register(FMLJavaModLoadingContext.get().getModEventBus());
         ItemInit.ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
         SoundsInit.SOUNDS.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
